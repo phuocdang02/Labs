f28447deda562c62382d82567443b806aad22c7a
Dang Ha <hpdang@mmsofts.com>
2024-11-27 17:09:16 +0700

    update DbContext

--- ECenterFlow/DbContexts/ECenterFlowDbContext.cs
+++ ECenterFlow/DbContexts/ECenterFlowDbContext.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ECenterFlow.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECenterFlow.DbContexts
 {
-    public class ECenterFlowDbContext: DbContext
+    public class ECenterFlowDbContext : DbContext
     {
-        public ECenterFlowDbContext(DbContextOptions<ECenterFlowDbContext> options): base(options) { }
+        public DbSet<Teachers> Teachers { get; set; }
+        public DbSet<Classes> Classes { get; set; }
+        public DbSet<Students> Students { get; set; }
+        public DbSet<Rooms> Rooms { get; set; }
+        public DbSet<Courses> Courses { get; set; }
+        public DbSet<Levels> Levels { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ECenterFlowDbContext"/> class.
+        /// </summary>
+        /// <param name="options">The DbContext options for configuring the database context.</param>
+        public ECenterFlowDbContext(DbContextOptions<ECenterFlowDbContext> options) : base(options) { }
     }
 }
